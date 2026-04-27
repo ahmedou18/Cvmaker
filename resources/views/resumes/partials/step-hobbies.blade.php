f3d7dce9ce2ed1a536b70d9dd644f4294df13cf8
ahmedou18 <101067514+ahmedou18@users.noreply.github.com>
2026-04-27 15:41:02 +0000

    Create step-hobbies.blade.php

---
+++ resources/views/resumes/partials/step-hobbies.blade.php
@@ -0,0 +1,22 @@
+@props(['currentLang' => 'ar'])
+
+<div x-show="step === 6" x-transition>
+    <div class="sharp-card">
+        <h2 class="text-2xl font-black mb-8 border-b pb-4">{{ __('messages.step_hobbies', [], $currentLang) ?? 'الهوايات والاهتمامات' }}</h2>
+        <template x-for="(hobby, idx) in hobbiesArray" :key="hobby.id">
+            <div class="flex flex-wrap gap-3 mb-4 items-center bg-gray-50 p-3 rounded">
+                <div class="w-20">
+                    <input type="text" :name="'hobbies['+idx+'][icon]'" x-model="hobby.icon" class="sharp-input text-center" placeholder="📚">
+                </div>
+                <div class="flex-1">
+                    <input type="text" :name="'hobbies['+idx+'][name]'" x-model="hobby.name" class="sharp-input w-full" placeholder="الهواية (مثال: القراءة)">
+                </div>
+                <div class="flex-1">
+                    <input type="text" :name="'hobbies['+idx+'][description]'" x-model="hobby.description" class="sharp-input w-full" placeholder="وصف قصير (اختياري)">
+                </div>
+                <button type="button" @click="hobbiesArray.splice(idx,1)" class="text-red-600 px-3 py-2">✕</button>
+            </div>
+        </template>
+        <button type="button" @click="addHobby()" class="sharp-btn-secondary mt-2">+ {{ __('messages.add_hobby', [], $currentLang) ?? 'أضف هواية' }}</button>
+    </div>
+</div>
