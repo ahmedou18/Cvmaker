2f298ee223ea74ce31ebda7a7db663dabf8f2d14
ahmedou18 <101067514+ahmedou18@users.noreply.github.com>
2026-04-28 12:00:51 +0000

    Update edit.blade.php

--- resources/views/resumes/edit.blade.php
+++ resources/views/resumes/edit.blade.php
@@ -2,6 +2,16 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css" rel="stylesheet">
     @include('resumes.partials.styles')
 
+@if($errors->any())
+<div class="mb-4 p-4 bg-red-100 text-red-800 rounded">
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
     @php
         $personal = $resume->personalDetail;
         $currentLang = $resume->resume_language ?? app()->getLocale();
