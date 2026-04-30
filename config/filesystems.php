71c4f2008910076b0b3768415edc81306c0b0f17
ahmedou18 <101067514+ahmedou18@users.noreply.github.com>
2026-04-30 11:18:16 +0000

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,13 @@ return [
             'report' => false,
         ],
 
+'template_thumbnails' => [
+    'driver' => 'local',
+    'root' => public_path('assets/images/templates'),
+    'url' => env('APP_URL').'/assets/images/templates',
+    'visibility' => 'public',
+],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
